6309bab5f8a4f01807186d89d5d9d81ac4881903
Abda Lodj <45307880+Abdalodj@users.noreply.github.com>
2020-12-17 08:02:48 +0000

    improving Servlet NewItems.java

--- src/java/com/colobane/Servlets/NewItems.java
+++ src/java/com/colobane/Servlets/NewItems.java
@@ -1,20 +1,86 @@
-package com.colobane.Servlets;
+package com.colobane.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.colobane.Bdd.ArticleQueries;
+import com.colobane.Beans.Article;
+import com.colobane.utils.ArticleOrder;
+import com.colobane.utils.CartController;
+
 public class NewItems extends HttpServlet {
 
-    public static final String VUE = "/WEB-INF/newItems.jsp";
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final String VUE = "/WEB-INF/newItems.jsp";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+    	ArticleQueries queries = new ArticleQueries();
+    	List<Article> articles = new ArrayList<Article>();
+    	CartController cartController = new CartController();
+    	
+    	int numberPage = 0;
+    	String action = (request.getParameter("action") != null)? request.getParameter("action") : "";
+    	request.setAttribute("count", cartController.cartSize(cartController.getCookieValue(request, "cart")));
+    	switch (action) {
+		case "lower": 
+			queries = new ArticleQueries();
+	    	articles = new ArrayList<Article>(); 
+	    	articles = queries.getArticlesLimitedOrderedBy(64, ArticleOrder.PRIX_Asc.ord+" , "+ArticleOrder.DATE_CREATION_Desc.ord);
+	    	request.setAttribute("articles", articles);
+	    	numberPage = (int) Math.ceil(48.0/16);
+	    	request.setAttribute("page", numberPage);
+	    	request.setAttribute("sort", 1);
+	    	this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+			break;
+		case "upper":
+			queries = new ArticleQueries();
+	    	articles = new ArrayList<Article>(); 
+	    	articles = queries.getArticlesLimitedOrderedBy(64, ArticleOrder.PRIX_Desc.ord+" , "+ArticleOrder.DATE_CREATION_Desc.ord);
+	    	request.setAttribute("articles", articles);
+	    	numberPage = (int) Math.ceil(48.0/16);
+	    	request.setAttribute("page", numberPage); 
+	    	request.setAttribute("sort", 2);
+	    	this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+			break;
+		case "atoz":
+			queries = new ArticleQueries();
+	    	articles = new ArrayList<Article>(); 
+	    	articles = queries.getArticlesLimitedOrderedBy(64, ArticleOrder.NOM_Asc.ord+" , "+ArticleOrder.DATE_CREATION_Desc.ord);
+	    	request.setAttribute("articles", articles);
+	    	numberPage = (int) Math.ceil(48.0/16);
+	    	request.setAttribute("page", numberPage);  
+	    	request.setAttribute("sort", 3);
+	    	this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+			break;
+		case "ztoa":
+			queries = new ArticleQueries();
+	    	articles = new ArrayList<Article>(); 
+	    	articles = queries.getArticlesLimitedOrderedBy(64, ArticleOrder.NOM_Desc.ord+" , "+ArticleOrder.DATE_CREATION_Desc.ord);
+	    	request.setAttribute("articles", articles);
+	    	numberPage = (int) Math.ceil(48.0/16);
+	    	request.setAttribute("page", numberPage);  
+	    	request.setAttribute("sort", 4); 
+	    	this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+			break;
+		default:
+	    	articles = queries.getArticlesLimitedOrderedBy(64, ArticleOrder.DATE_CREATION_Desc.ord);
+	    	request.setAttribute("articles", articles);
+	    	numberPage = (int) Math.ceil(64.0/16);
+	    	request.setAttribute("page", numberPage);  
+	    	this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+			break;
+		}   
     }
 
     @Override
