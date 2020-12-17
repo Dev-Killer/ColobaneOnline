5ce9f0fcb2fc19a843004eb9f1e3638c443d1f9a
Abda Lodj <45307880+Abdalodj@users.noreply.github.com>
2020-12-17 08:01:58 +0000

    improving Servlet Categories.java

--- src/java/com/colobane/Servlets/Categories.java
+++ src/java/com/colobane/Servlets/Categories.java
@@ -1,19 +1,31 @@
-package com.colobane.Servlets;
+package com.colobane.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.colobane.utils.CartController;
+import com.colobane.utils.CategoriesController;
+
 public class Categories extends HttpServlet {
 
-    public static final String VUE = "/WEB-INF/category.jsp";
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final String VUE = "/WEB-INF/category.jsp";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+    	CategoriesController categController = new CategoriesController();
+    	CartController cartController = new CartController();
+    	request = categController.getResponse(request);
+    	request.setAttribute("count", cartController.cartSize(cartController.getCookieValue(request, "cart"))); 
+    	this.getServletContext().getRequestDispatcher(VUE).forward(request, response);    	
     }
 
     @Override
