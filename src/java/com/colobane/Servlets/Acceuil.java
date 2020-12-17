222f57d66d822ab78807ce5f26cc210313b5bae2
Abda Lodj <45307880+Abdalodj@users.noreply.github.com>
2020-12-17 08:02:20 +0000

    improving Servlet Accueil.java

--- src/java/com/colobane/Servlets/Acceuil.java
+++ src/java/com/colobane/Servlets/Acceuil.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.colobane.Bdd.ArticleQueries;
 import com.colobane.Beans.Article;
+import com.colobane.utils.ArticleOrder;
+import com.colobane.utils.CartController;
 
 public class Acceuil extends HttpServlet {
 
@@ -23,30 +26,53 @@ public class Acceuil extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		List<Article> colArticles = new ArrayList<Article>();
+		List<Article> litArticles = new ArrayList<Article>();
+		List<Article> compil1Articles = new ArrayList<Article>();
+		List<Article> compil2Articles = new ArrayList<Article>();
+		ArticleQueries queries = new ArticleQueries();
+		CartController cartController = new CartController(); 
+
+		colArticles = queries.getArticlesByPriceRangeLimitedOrderedBy(15000, 50000, ArticleOrder.DATE_CREATION_Desc.ord,
+				10);
+		litArticles = queries.getArticlesLimitedOrderedBy(10, ArticleOrder.DESCRIPTION_Asc.ord);
+		compil1Articles = queries.getArticlesByPriceRangeLimitedOrderedBy(1000, 14900, ArticleOrder.PRIX_Desc.ord, 10);
+		compil2Articles = queries.getArticlesByPriceRangeLimitedOrderedBy(60000, 90000, null, 10);
+		int cartSize = cartController.cartSize(cartController.getCookieValue(request, "cart"));
+ 
+		request.setAttribute("cols", colArticles);
+		request.setAttribute("lits", litArticles);
+		request.setAttribute("compil1s", compil1Articles);
+		request.setAttribute("compil2s", compil2Articles);
+		request.setAttribute("count", cartSize);
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-//    	String action = request.getParameter("action");
-//    	switch (action) {
-//		case "ajax":
-//			String idP = request.getParameter("param1");
-//			
-//			Cookie cookie = new Cookie("cart", idP+";");
-//			response.addCookie(cookie);
-//			int reponse = 12;   
-//			response.getWriter().println(reponse); 
-//			break;
-//
-//		default:
-		ArticleQueries articleQueries = new ArticleQueries();
+		String action = request.getParameter("action");
+		switch (action) {
+		case "ajax":
+			String idP = request.getParameter("param1");
+			CartController cartController = new CartController();
+			String cart = cartController.getCookieValue(request, "cart");   
+			cart = cartController.cartListString(cart, idP);
+			Cookie cookie = new Cookie("cart", cart);
+			cookie.setMaxAge(60 * 60 * 24 * 365);
+			response.addCookie(cookie);
+			int rep = cartController.cartSize(cart);
+			response.getWriter().println(rep);
+			break;
 
-		List<Article> articles = new ArrayList<Article>();
-		articles = articleQueries.getAllArticlesList();
-		request.setAttribute("articles", articles);
+		default:
+			ArticleQueries articleQueries = new ArticleQueries();
 
-		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+			List<Article> articles = new ArrayList<Article>();
+			articles = articleQueries.getAllArticlesList();
+			request.setAttribute("articles", articles);
+
+			this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+		}
 	}
 }
